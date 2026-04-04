5780cdc420b084a7da49d9e37e6e2bda17f20fdd
Wasim Akram <w1409632@gmail.com>
2026-04-04 22:25:01 +0530

    Test clicking add to cart button

--- src/pages/Home/product.test.jsx
+++ src/pages/Home/product.test.jsx
@@ -1,6 +1,9 @@
 import { it, expect, describe,vi } from 'vitest'
 import { Product } from './product';
 import { render,screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import axios from 'axios';
+vi.mock('axios')
 
 describe('product componet', () => {
   it('displays the product details correctly', () => {
@@ -16,7 +19,7 @@ describe('product componet', () => {
       keywords: ["socks", "sports", "apparel"]
     }
     const loadCart=vi.fn();
-    render(<Product product={product} loadcart={loadCart}/>)
+    render(<Product product={product} loadCart={loadCart}/>)
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
 
     expect(screen.getByText('$10.90')).toBeInTheDocument()
@@ -24,8 +27,34 @@ describe('product componet', () => {
 
     expect(screen.getByTestId('product-rating-stars-image')).toHaveAttribute('src','images/ratings/rating-45.png')
 
-    expect(screen.getByText('87')).toBeInTheDocument()
+    expect(screen.getByText('87')).toBeInTheDocument() 
 
   })
 
+  it('add a product to the cart', async()=>{
+    const product = {
+      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      rating: {
+        stars: 4.5,
+        count: 87
+      },
+      priceCents: 1090,
+      keywords: ["socks", "sports", "apparel"]
+    }
+    const loadCart=vi.fn();
+    render(<Product product={product} loadCart={loadCart}/>)
+    const user =userEvent.setup();
+    const addToCartButton = screen.getByTestId('add-to-cart-button')
+
+    await user.click(addToCartButton);
+    expect(axios.post).toHaveBeenCalledWith('/api/cart-items',
+      {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    });
+    expect(loadCart).toHaveBeenCalled();
+    
+  })
 })
